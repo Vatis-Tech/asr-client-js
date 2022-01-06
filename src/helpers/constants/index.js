29b63554cb56ceb9ab43c29a1d657781a06bf9da
einazare <emanuelioannazare@gmail.com>
2022-01-06 17:02:15 +0200

    VAT-243: add constants file

---
+++ src/helpers/constants/index.js
@@ -0,0 +1,8 @@
+const API_URL =
+  "https://vatis.tech/api/v1/asr-client/auth?service=<service>&model=<model>&language=<language>";
+
+const projectConstants = {
+  API_URL,
+};
+
+export default projectConstants;
