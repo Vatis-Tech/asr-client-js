81df130cfd730c57af3f0235e0d6d4ae00033cf3
einazare <emanuelioannazare@gmail.com>
2022-01-11 15:37:19 +0200

    VAT-243: refactor and add code for socket io communication

--- src/components/SocketIOClientGenerator.js
+++ src/components/SocketIOClientGenerator.js
@@ -2,15 +2,20 @@ import io from "socket.io-client";
 
 class SocketIOClientGenerator {
   socketRef;
-  constructor({
-    serviceHost,
-    authToken,
-    onConnectCallback,
-    onAsrResultCallback,
-  }) {
-    const serviceHost = `${serviceHost}/asr_stream`;
-    this.socketRef = io(serviceHost, {
-      path: "/live/ro_RO/transcribe/socket.io",
+  serviceHost;
+  authToken;
+  onConnectCallback;
+  onAsrResultCallback;
+  constructor({ onConnectCallback, onAsrResultCallback }) {
+    this.onConnectCallback = onConnectCallback;
+    this.onAsrResultCallback = onAsrResultCallback;
+  }
+  init({ serviceHost, authToken }) {
+    this.serviceHost = serviceHost;
+    this.authToken = authToken;
+    const serviceHostStream = `${serviceHost}/asr_stream`;
+    this.socketRef = io(serviceHostStream, {
+      path: "/asr/v1/live/transcribe/socket.io",
       transports: ["polling", "websocket"],
       namespace: "/asr_stream",
       extraHeaders: {
@@ -23,6 +28,15 @@ class SocketIOClientGenerator {
     this.socketRef.on("connect", () => {
       this.onConnectCallback();
     });
+    this.socketRef.on("connect_error", (error) => {
+      const errorMessage =
+        'Could not initilize the "socket.io-client" with error: ' + error;
+      console.error(errorMessage);
+      throw errorMessage;
+    });
+    this.socketRef.on("/asr_result", (args) => {
+      this.onAsrResultCallback(args);
+    });
     // TODO: add some callbacks for all states
     // NOTE: this would be usefull for end users to know the state of
     // NOTE: the Vatis Tech Client plugin
@@ -34,14 +48,10 @@ class SocketIOClientGenerator {
     // this.socketRef.on("reconnect", () => {
     //   console.log("reconnect");
     // });
-    this.socketRef.on("connect_error", (error) => {
-      const errorMessage =
-        'Could not initilize the "socket.io-client" with error: ' + error;
-      console.error(errorMessage);
-      throw errorMessage;
-    });
-    this.socketRef.on("/asr_result", (args) => {
-      this.onAsrResultCallback(args);
+  }
+  emitData(data) {
+    this.socketRef.emit("/asr_request", {
+      data,
     });
   }
 }
