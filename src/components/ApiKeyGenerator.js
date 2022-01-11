6b10038e77c1d86e839f67572d7dfe565548bcf7
einazare <emanuelioannazare@gmail.com>
2022-01-11 15:02:49 +0200

    VAT-243: refactor API generator

--- src/components/ApiKeyGenerator.js
+++ src/components/ApiKeyGenerator.js
@@ -2,21 +2,33 @@ class ApiKeyGenerator {
   apiUrl;
   responseCallback;
   apiKey;
+  serviceHost;
+  authToken;
+  xmlHttp;
   constructor({ apiUrl, responseCallback, apiKey }) {
     this.apiUrl = apiUrl;
     this.responseCallback = responseCallback;
     this.apiKey = apiKey;
-    let xmlHttp = new XMLHttpRequest();
-    xmlHttp.addEventListener("load", function () {
-      const credentials = JSON.parse(this.responseText);
-      responseCallback({
-        serviceHost: credentials.service_host,
-        authToken: credentials.auth_token,
-      });
+    this.xmlHttp = new XMLHttpRequest();
+    this.xmlHttp.onload = this.onLoad.bind(this);
+    this.xmlHttp.onerror = this.onError;
+    this.xmlHttp.open("GET", apiUrl);
+    this.xmlHttp.setRequestHeader("Authorization", "Bearer " + apiKey);
+    this.xmlHttp.send();
+  }
+  onError(e) {
+    const errorMessage = `Could not initilize the API KEY.`;
+    console.error(errorMessage);
+    throw errorMessage;
+  }
+  onLoad() {
+    const credentials = JSON.parse(this.xmlHttp.responseText);
+    this.serviceHost = credentials.service_host;
+    this.authToken = credentials.auth_token;
+    this.responseCallback({
+      serviceHost: credentials.service_host,
+      authToken: credentials.auth_token,
     });
-    xmlHttp.open("GET", apiUrl);
-    xmlHttp.setRequestHeader("Authorization", "Bearer " + apiKey);
-    xmlHttp.send();
   }
 }
 
