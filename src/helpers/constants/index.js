ff9847cddd4412a48b840aa5d2a954bd7f7245fa
einazare <emanuelioannazare@gmail.com>
2022-01-11 16:28:32 +0200

    VAT-243: add Socket IO Client constants
    TODO: check the FrameLength, FrameOverlap and BufferOffset how to make 
    them constants as well, or generate them based on the 
    MicrophoneGenerator bufferSize.

--- src/helpers/constants/index.js
+++ src/helpers/constants/index.js
@@ -1,8 +1,19 @@
 const API_URL =
-  "https://vatis.tech/api/v1/asr-client/auth?service=<service>&model=<model>&language=<language>";
+  "http://localhost:8080/api/v1/asr-client/auth?service=<service>&model=<model>&language=<language>";
+
+const SOCKET_IO_CLIENT_NAMESPACE = "/asr_stream";
+const SOCKET_IO_CLIENT_TRANSPORTS = ["polling", "websocket"];
+const SOCKET_IO_CLIENT_PATH = "/asr/v1/live/transcribe/socket.io";
+const SOCKET_IO_CLIENT_RESULT_PATH = "/asr_result";
+const SOCKET_IO_CLIENT_REQUEST_PATH = "/asr_request";
 
 const projectConstants = {
   API_URL,
+  SOCKET_IO_CLIENT_NAMESPACE,
+  SOCKET_IO_CLIENT_TRANSPORTS,
+  SOCKET_IO_CLIENT_PATH,
+  SOCKET_IO_CLIENT_RESULT_PATH,
+  SOCKET_IO_CLIENT_REQUEST_PATH,
 };
 
 export default projectConstants;
