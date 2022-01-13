e124c45eece19b0b0d37d8f1c22f06781dca5c18
einazare <emanuelioannazare@gmail.com>
2022-01-13 17:49:58 +0200

    VAT-243: add new constants for the socket communications

--- src/helpers/constants/index.js
+++ src/helpers/constants/index.js
@@ -6,6 +6,8 @@ const SOCKET_IO_CLIENT_TRANSPORTS = ["polling", "websocket"];
 const SOCKET_IO_CLIENT_PATH = "/asr/v1/live/transcribe/socket.io";
 const SOCKET_IO_CLIENT_RESULT_PATH = "/asr_result";
 const SOCKET_IO_CLIENT_REQUEST_PATH = "/asr_request";
+const SOCKET_IO_CLIENT_RESPONSE_SPLIT_PACKET = "SplitPacket";
+const SOCKET_IO_CLIENT_RESPONSE_FINAL_SPLIT_PACKET = "FinalSplitPacket";
 
 const projectConstants = {
   API_URL,
