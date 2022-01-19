c7b6d3177d677e0469a80d9d1b6ff08f996ce45b
einazare <emanuelioannazare@gmail.com>
2022-01-19 15:32:25 +0200

    VAT-243: add logger
    Added this for better understaning the state of the plugin.
    
    NOTE: this may change in the future with something better.

--- src/index.js
+++ src/index.js
@@ -19,7 +19,24 @@ class VatisTechClient {
   microphoneQueue;
   onData;
   waitingForFinalPacket;
-  constructor({ service, model, language, apiKey, onData }) {
+  logger;
+  log;
+  constructor({ service, model, language, apiKey, onData, log, logger }) {
+    this.log = log;
+
+    if (this.log === true && typeof logger === "function") {
+      this.logger = logger;
+    } else if (this.log === true) {
+      this.logger = console.log;
+    } else {
+      this.logger = () => {};
+    }
+
+    this.logger({
+      currentState: `@vatis-tech/asr-client-js: Initilizing plugin.`,
+      description: `@vatis-tech/asr-client-js: This is the base constructor which initilizez everything for the LIVE ASR service of Vatis Tech.`,
+    });
+
     // this is a flag that says if the whole response for the previous packet was received or not
     this.waitingForFinalPacket = false;
 
@@ -27,13 +44,16 @@ class VatisTechClient {
     this.onData = onData;
 
     // instantiante MicrophoneQueue - this will keep all the microphone buffers until they can be sent to the ASR SERVICE through the SocketIOClientGenerator
-    this.microphoneQueue = new MicrophoneQueue();
+    this.microphoneQueue = new MicrophoneQueue({
+      logger: this.logger.bind(this),
+    });
 
     // instantiante ApiKeyGenerator - this will return on the responseCallback the serviceHost and the authToken for the SocketIOClientGenerator to connect based on the apiUrl and apiKey
     this.apiKeyGenerator = new ApiKeyGenerator({
       apiUrl: generateApiUrl({ service, model, language }),
       responseCallback: this.initSocketIOClient.bind(this),
       apiKey: apiKey,
+      logger: this.logger.bind(this),
     });
 
     // instantiante SocketIOClientGenerator - this will return on the onAsrResultCallback the results that it gets back from the ASR SERVICE and when it connects to the ASR SERVICE it will initilize the MicrophoneGenerator through the onConnectCallback
@@ -41,11 +61,13 @@ class VatisTechClient {
       onConnectCallback: this.initMicrophone.bind(this),
       onAsrResultCallback:
         this.onSocketIOClientGeneratorOnAsrResultCallback.bind(this),
+      logger: this.logger.bind(this),
     });
 
     // instantiante MicrophoneGenerator - this will return on the this.onMicrophoneGeneratorDataCallback the data that it captures from the user's microphone
     this.microphoneGenerator = new MicrophoneGenerator({
       onDataCallback: this.onMicrophoneGeneratorDataCallback.bind(this),
+      logger: this.logger.bind(this),
     });
 
     // initilize ApiKeyGenerator (if successful it will initilize SocketIOClientGenerator (if successful it will initilize the MicrophoneGenerator))
