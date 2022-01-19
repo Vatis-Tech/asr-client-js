e8d8fae57de9b85029ce1387ac190e87f00c023e
einazare <emanuelioannazare@gmail.com>
2022-01-19 15:31:44 +0200

    VAT-243: add logger
    Added this for better understaning the state of the plugin.
    
    NOTE: this may change in the future with something better.

--- src/components/MicrophoneQueue.js
+++ src/components/MicrophoneQueue.js
@@ -1,6 +1,14 @@
 class MicrophoneQueue {
   queue;
-  constructor() {
+  logger;
+  constructor({ logger }) {
+    this.logger = logger;
+
+    this.logger({
+      currentState: `@vatis-tech/asr-client-js: Instantianting and initializing the "MicrophoneQueue" plugin.`,
+      description: `@vatis-tech/asr-client-js: The data from user's microphone is stored here. When the SocketIOClientGenerator gets a response for the previous sent data, it takes from here the next data that was recored by the MicrophoneGenerator and sends it to the LIVE ASR service.`,
+    });
+
     this.queue = [];
   }
   dequeue() {
