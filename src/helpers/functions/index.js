c386d2a87251f6fae98ccccb4f2636484b17e2c6
einazare <emanuelioannazare@gmail.com>
2022-02-02 12:50:23 +0200

    VAT-243: add converter from ArrayBuffer to Base64 helper function

--- src/helpers/functions/index.js
+++ src/helpers/functions/index.js
@@ -1,9 +1,11 @@
 import checkIfFinalPacket from "./checkIfFinalPacket.js";
 import generateApiUrl from "./generateApiUrl.js";
+import base64ArrayBuffer from "./base64ArrayBuffer.js";
 
 const functions = {
   checkIfFinalPacket,
   generateApiUrl,
+  base64ArrayBuffer,
 };
 
 export default functions;
