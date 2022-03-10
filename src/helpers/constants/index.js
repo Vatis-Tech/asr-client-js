d9c6fe133b4f1df21e0dbfa6f3bd703b0d625847
einazare <emanuelioannazare@gmail.com>
2022-03-10 13:19:45 +0200

    VAT-243: delete polling

--- src/helpers/constants/index.js
+++ src/helpers/constants/index.js
@@ -2,7 +2,7 @@ const API_URL =
   "https://vatis.tech/api/v1/asr-client/auth?service=<service>&model=<model>&language=<language>";
 
 const SOCKET_IO_CLIENT_NAMESPACE = "/asr_stream";
-const SOCKET_IO_CLIENT_TRANSPORTS = ["polling", "websocket"];
+const SOCKET_IO_CLIENT_TRANSPORTS = ["websocket"];
 const SOCKET_IO_CLIENT_PATH = "/asr/v1/live/transcribe/socket.io";
 const SOCKET_IO_CLIENT_RESULT_PATH = "/asr_result";
 const SOCKET_IO_CLIENT_REQUEST_PATH = "/asr_request";
