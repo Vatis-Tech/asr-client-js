a882517c83128a2550f1d07f7088ff704079162d
einazare <emanuelioannazare@gmail.com>
2022-03-17 15:23:28 +0200

    VAT-243: fix reservation url constant

--- dist/helpers/constants/index.js
+++ dist/helpers/constants/index.js
@@ -5,7 +5,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = void 0;
 var API_URL = "http://localhost:8080/api/v1/asr-client/auth?service=<service>&model=<model>&language=<language>";
-var RESERVATION_URL = "http://<service_host>/asr/v1/registry/stream/reserve";
+var RESERVATION_URL = "<service_host>/asr/v1/registry/stream/reserve";
 var SOCKET_IO_CLIENT_NAMESPACE = "/asr_stream";
 var SOCKET_IO_CLIENT_TRANSPORTS = ["websocket"]; // const SOCKET_IO_CLIENT_PATH = "/asr/v1/live/transcribe/socket.io";
 
