fc3ec1b3380ad0ca3e59436a44dddb3aeb55d9fe
einazare <emanuelioannazare@gmail.com>
2022-03-17 13:58:53 +0200

    VAT-243: add reservation

--- dist/helpers/functions/index.js
+++ dist/helpers/functions/index.js
@@ -11,12 +11,15 @@ var _generateApiUrl = _interopRequireDefault(require("./generateApiUrl.js"));
 
 var _base64ArrayBuffer = _interopRequireDefault(require("./base64ArrayBuffer.js"));
 
+var _generateReservationUrl = _interopRequireDefault(require("./generateReservationUrl.js"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var functions = {
   checkIfFinalPacket: _checkIfFinalPacket["default"],
   generateApiUrl: _generateApiUrl["default"],
-  base64ArrayBuffer: _base64ArrayBuffer["default"]
+  base64ArrayBuffer: _base64ArrayBuffer["default"],
+  generateReservationUrl: _generateReservationUrl["default"]
 };
 var _default = functions;
 exports["default"] = _default;
