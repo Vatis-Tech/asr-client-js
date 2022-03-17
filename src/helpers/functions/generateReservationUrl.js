91cf0830958ee159591e02824b19dd0cc5031662
einazare <emanuelioannazare@gmail.com>
2022-03-17 13:57:57 +0200

    VAT-243: add reservation link generator function

---
+++ src/helpers/functions/generateReservationUrl.js
@@ -0,0 +1,13 @@
+import constants from "../constants/index.js";
+
+const { RESERVATION_URL } = constants;
+
+function generateReservationUrl({ serviceHost }) {
+  let reservationUrl = RESERVATION_URL;
+
+  reservationUrl.replace("<service_host>", serviceHost);
+
+  return reservationUrl;
+}
+
+export default generateReservationUrl;
