632c9101fc2b6cd55d207da66b2a538a9fd8d0f0
einazare <emanuelioannazare@gmail.com>
2023-01-18 13:13:24 +0200

    VAT-988: fix missing export

--- src/helpers/functions/index.js
+++ src/helpers/functions/index.js
@@ -2,12 +2,14 @@ import checkIfFinalPacket from "./checkIfFinalPacket.js";
 import generateApiUrl from "./generateApiUrl.js";
 import base64ArrayBuffer from "./base64ArrayBuffer.js";
 import generateReservationUrl from "./generateReservationUrl.js";
+import checkIfFinalPacket from "./checkIfFinalPacket.js";
 
 const functions = {
   checkIfFinalPacket,
   generateApiUrl,
   base64ArrayBuffer,
   generateReservationUrl,
+  checkIfFinalPacket,
 };
 
 export default functions;
