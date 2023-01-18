30f3a3a3de071de6ecd0ff7042b7fac433f01d61
einazare <emanuelioannazare@gmail.com>
2023-01-18 12:01:16 +0200

    VAT-988: add first message to be of config

--- src/components/MicrophoneQueue.js
+++ src/components/MicrophoneQueue.js
@@ -1,15 +1,26 @@
 class MicrophoneQueue {
   queue;
   logger;
-  constructor({ logger }) {
+  config;
+  constructor({ logger, config }) {
     this.logger = logger;
+    this.config = config;
 
     this.logger({
       currentState: `@vatis-tech/asr-client-js: Instantianting and initializing the "MicrophoneQueue" plugin.`,
       description: `@vatis-tech/asr-client-js: The data from user's microphone is stored here. When the SocketIOClientGenerator gets a response for the previous sent data, it takes from here the next data that was recored by the MicrophoneGenerator and sends it to the LIVE ASR service.`,
     });
 
-    this.queue = [];
+    if (config) {
+      this.queue = [
+        {
+          type: SOCKET_IO_CLIENT_MESSAGE_TYPE_CONFIG,
+          ...config
+        }
+      ];
+    } else {
+      this.queue = [];
+    }
   }
   dequeue() {
     return this.queue.shift(0, 1);
