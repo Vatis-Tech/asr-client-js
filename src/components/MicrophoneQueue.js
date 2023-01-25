954db789126354005a0f4908742841cec5aae512
einazare <emanuelioannazare@gmail.com>
2023-01-25 15:35:34 +0200

    VAT-988: move config to socket

--- src/components/MicrophoneQueue.js
+++ src/components/MicrophoneQueue.js
@@ -1,30 +1,15 @@
-import projectConstants from "../helpers/constants";
-
-const { SOCKET_IO_CLIENT_MESSAGE_TYPE_CONFIG } = projectConstants;
-
 class MicrophoneQueue {
   queue;
   logger;
-  config;
-  constructor({ logger, config }) {
+  constructor({ logger }) {
     this.logger = logger;
-    this.config = config;
 
     this.logger({
       currentState: `@vatis-tech/asr-client-js: Instantianting and initializing the "MicrophoneQueue" plugin.`,
       description: `@vatis-tech/asr-client-js: The data from user's microphone is stored here. When the SocketIOClientGenerator gets a response for the previous sent data, it takes from here the next data that was recored by the MicrophoneGenerator and sends it to the LIVE ASR service.`,
     });
 
-    if (config) {
-      this.queue = [
-        {
-          type: SOCKET_IO_CLIENT_MESSAGE_TYPE_CONFIG,
-          ...config
-        }
-      ];
-    } else {
-      this.queue = [];
-    }
+    this.queue = [];
   }
   dequeue() {
     return this.queue.shift(0, 1);
