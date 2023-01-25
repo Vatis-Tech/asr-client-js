bcb05ee6f91f17de1e9c773425bb3e5b53f1ad63
einazare <emanuelioannazare@gmail.com>
2023-01-25 15:14:42 +0200

    deploy

--- dist/cjs/components/MicrophoneQueue.js
+++ dist/cjs/components/MicrophoneQueue.js
@@ -8,8 +8,10 @@ exports["default"] = void 0;
 var _classCallCheck2 = _interopRequireDefault(require("@babel/runtime/helpers/classCallCheck"));
 var _createClass2 = _interopRequireDefault(require("@babel/runtime/helpers/createClass"));
 var _defineProperty2 = _interopRequireDefault(require("@babel/runtime/helpers/defineProperty"));
+var _constants = _interopRequireDefault(require("../helpers/constants"));
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { (0, _defineProperty2["default"])(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
+var SOCKET_IO_CLIENT_MESSAGE_TYPE_CONFIG = _constants["default"].SOCKET_IO_CLIENT_MESSAGE_TYPE_CONFIG;
 var MicrophoneQueue = /*#__PURE__*/function () {
   function MicrophoneQueue(_ref) {
     var logger = _ref.logger,
