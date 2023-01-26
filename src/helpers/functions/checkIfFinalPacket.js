890f61c4c95231c39023166a82dbbaeabe758c30
einazare <emanuelioannazare@gmail.com>
2023-01-26 16:41:40 +0200

    VAT-981: revert code

--- src/helpers/functions/checkIfFinalPacket.js
+++ src/helpers/functions/checkIfFinalPacket.js
@@ -1,13 +1,19 @@
 import constants from "../constants/index.js";
 
 const {
-  SOCKET_IO_CLIENT_RESPONSE_FINAL_PACKET
+  SOCKET_IO_CLIENT_RESPONSE_SPLIT_PACKET,
+  SOCKET_IO_CLIENT_RESPONSE_FINAL_SPLIT_PACKET,
 } = constants;
 
 const checkIfFinalPacket = (data) => {
   return (
-    data.headers.hasOwnProperty(SOCKET_IO_CLIENT_RESPONSE_FINAL_PACKET) &&
-    data.headers[SOCKET_IO_CLIENT_RESPONSE_FINAL_PACKET] === true
+    !data.headers.hasOwnProperty(SOCKET_IO_CLIENT_RESPONSE_SPLIT_PACKET) ||
+    (data.headers.hasOwnProperty(SOCKET_IO_CLIENT_RESPONSE_SPLIT_PACKET) &&
+      data.headers.hasOwnProperty(
+        SOCKET_IO_CLIENT_RESPONSE_FINAL_SPLIT_PACKET
+      ) &&
+      data.headers[SOCKET_IO_CLIENT_RESPONSE_SPLIT_PACKET] === true &&
+      data.headers[SOCKET_IO_CLIENT_RESPONSE_FINAL_SPLIT_PACKET] === true)
   );
 };
 
