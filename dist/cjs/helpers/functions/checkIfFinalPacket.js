a2528ab3c04bcd9d84db8aca70837185c8df2bdb
einazare <emanuelioannazare@gmail.com>
2023-01-26 15:34:33 +0200

    deploy next

--- dist/cjs/helpers/functions/checkIfFinalPacket.js
+++ dist/cjs/helpers/functions/checkIfFinalPacket.js
@@ -9,7 +9,7 @@ var _index = _interopRequireDefault(require("../constants/index.js"));
 var SOCKET_IO_CLIENT_RESPONSE_SPLIT_PACKET = _index["default"].SOCKET_IO_CLIENT_RESPONSE_SPLIT_PACKET,
   SOCKET_IO_CLIENT_RESPONSE_FINAL_SPLIT_PACKET = _index["default"].SOCKET_IO_CLIENT_RESPONSE_FINAL_SPLIT_PACKET;
 var checkIfFinalPacket = function checkIfFinalPacket(data) {
-  return !data.headers.hasOwnProperty(SOCKET_IO_CLIENT_RESPONSE_SPLIT_PACKET) || data.headers.hasOwnProperty(SOCKET_IO_CLIENT_RESPONSE_SPLIT_PACKET) && data.headers.hasOwnProperty(SOCKET_IO_CLIENT_RESPONSE_FINAL_SPLIT_PACKET) && data.headers.SplitPacket === true && data.headers.FinalSplitPacket === true;
+  return data.headers.hasOwnProperty(SOCKET_IO_CLIENT_RESPONSE_FINAL_PACKET) && data.headers[SOCKET_IO_CLIENT_RESPONSE_FINAL_PACKET] === true;
 };
 var _default = checkIfFinalPacket;
 exports["default"] = _default;
